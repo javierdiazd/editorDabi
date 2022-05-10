49240839b7664ff9990503d9912b30506b447e35
javierdiazd <65363405+javierdiazd@users.noreply.github.com>
2022-05-09 22:25:10 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+const actualizar = () => {
+    const texto_ingresado = document.getElementById("texto_ingresado");
+    const editor = document.getElementById("editor");
+    editor.srcdoc = texto_ingresado.value;
+}
